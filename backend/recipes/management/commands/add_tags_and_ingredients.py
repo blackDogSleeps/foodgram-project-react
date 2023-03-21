5f6f756fcd445642ce91f4a371d0d68cd357ba82
blackDogSleeps <basilkooryshev@gmail.com>
2023-03-21 14:09:45 +0300

    Tags are now added with 'python manage.py add_tags_and_ingredients' command

---
+++ backend/recipes/management/commands/add_tags_and_ingredients.py
@@ -0,0 +1,39 @@
+import json
+import os
+
+from django.core.management.base import BaseCommand, CommandError
+
+from recipes.models import Ingredient, Tag
+
+
+class Command(BaseCommand):
+    help = 'Добавляет ингридиенты и теги из папки "data" в базу данных'
+
+    def handle(self, *args, **options):
+        os.chdir('../data')
+        ingredients_file =  open('ingredients.json', encoding='UTF-8')
+        tags_file = open('tags.json', encoding='UTF-8')
+        
+        ingredients = json.load(ingredients_file)
+        tags = json.load(tags_file)
+
+        ingredients_file.close()
+        tags_file.close()
+
+        for i in ingredients:
+            obj = Ingredient(name=i.get('name'),
+                             measurement_unit=i.get('measurement_unit'))
+            if not Ingredient.objects.filter(name=obj.name).exists():
+                print(obj)
+                obj.save()
+
+        for i in tags:
+            obj = Tag(name=i.get('name'),
+                      color=i.get('color'),
+                      slug=i.get('slug'))
+            if not Tag.objects.filter(name=obj.name).exists():
+                print(obj)
+                obj.save()
+
+        
+
